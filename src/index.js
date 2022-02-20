e3e48b830e6e92cf8cb39b84f825f3edc628fd0e
Mohammad-S-Abualhaj <mohammad.s.abualhaj@gmail.com>
2022-02-20 11:01:02 +0200

    pull

--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './components/Home'
 import Room from './components/Room'
-
+import Contact from './components/Contact'
+import Thanks from './components/Thanks'
+import Hostel from './components/Hostel'
+import Error from './components/Error'
+import Gallery from './components/Gallery'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 ReactDOM.render(
@@ -11,7 +15,12 @@ ReactDOM.render(
 	<Header/>
 		<Routes>
 			<Route path="/" element={<Home/>}/>
-			<Route path="/room" element={<Room/>}/>
+			<Route path="/contact" element={<Room/>}/>
+			<Route path="/contact" element={<Contact/>}/>
+			<Route path="/404" element={<Error/>}/>
+			<Route path="/thanks" element={<Thanks/>}/>
+			<Route path="/hostel" element={<Hostel/>}/>
+			<Route path="/gallery" element={<Gallery/>}/>
 		</Routes>
 		<Footer/>
 	</BrowserRouter>,
