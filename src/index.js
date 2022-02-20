eadad4c99e68fd33999414e894160f6b9e475795
Mohammad-S-Abualhaj <mohammad.s.abualhaj@gmail.com>
2022-02-20 10:42:51 +0200

    convert to react v1

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './components/Home'
+import Room from './components/Room'
+
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+ReactDOM.render(
+	<BrowserRouter>
+	<Header/>
+		<Routes>
+			<Route path="/" element={<Home/>}/>
+			<Route path="/room" element={<Room/>}/>
+		</Routes>
+		<Footer/>
+	</BrowserRouter>,
+	document.getElementById("root")
+);
