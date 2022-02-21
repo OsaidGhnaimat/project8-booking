9d8a61df85717beb0a4ab8e793f7fd3bfab72d56
Sara-Hasan <sara.ahabash@gmail.com>
2022-02-21 19:56:32 +0200

    up

--- dashboard/order.php
+++ dashboard/order.php
@@ -1,5 +1,6 @@
 <?php 
 require 'connect.php';
+session_start();
 header("Access-Control-Allow-Origin: *");
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
